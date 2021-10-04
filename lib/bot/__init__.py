d2af2efbfa7ca736ccd093ce077ef98f323fe875
William F <48420172+willkj@users.noreply.github.com>
2021-10-04 20:17:58 -0300

    Update __init__.py

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,3 +1,4 @@
+from discord import Intents
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
 
@@ -11,7 +12,10 @@ class Bot(BotBase):
         self.guild = None
         self.scheduler =  AsyncIOScheduler()
 
-        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+        super().__init__(command_prefix=PREFIX,
+            owner_ids=OWNER_IDS,
+            intents=Intents.all(),
+        )
     
     def run(self, version):
         self.version = version
