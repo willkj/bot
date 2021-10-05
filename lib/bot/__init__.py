280e6120945ce19a1cda9312c1ba25ee057776d6
William F <48420172+willkj@users.noreply.github.com>
2021-10-05 11:33:30 -0300

    Update __init__.py

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,7 +1,8 @@
 from discord import Intents
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord import Embed
 from discord.ext.commands import Bot as BotBase
-
+from datetime import datetime
 PREFIX = "."
 OWNER_IDS = [313751695819538432]
 
@@ -18,7 +19,7 @@ class Bot(BotBase):
         )
     
     def run(self, version):
-        self.version = version
+        self.VERSION = version
 
         with open("./lib/bot/token", "r", encoding="utf-8") as tf:
             self.TOKEN = tf.read()
@@ -38,6 +39,26 @@ class Bot(BotBase):
             self.ready = True
             self.guild = self.get_guild(887668262941392896) 
             print("bot ready")
+
+            channel = self.get_channel(894574583892549712)
+            await channel.send("Online agora!")
+
+            embed = Embed(title="Online Agora!", description="Lif agora Online!", 
+                        colour=0xFF0000, timestamp=datetime.utcnow())
+            fields = [("Name", "Value", True),
+                    ("Another Field", "This Field is next to the other one", True),
+                    ("A non-inline field" , "This field will appear on it's own row", False)]
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+            embed.set_author(name="Will", icon_url=self.guild.icon_url)
+            embed.set_footer(text="This is a footer!")
+            embed.set_thumbnail(url=self.guild.icon_url)
+            embed.set_image(url=self.guild.icon_url)
+
+            await channel.send(embed=embed)
+            await channeld.send(file=File("./data/images/profile.png"))
+
+
 
         else:
             print("bot reconnected")
